da6e1372e635a72524fa6e538ec99587f39e4519
a.arkhipov <tyomaa88@gmail.com>
2017-11-24 21:33:19 +0300

    various

---
+++ gamejam_2/Assets/Scripts/Dance/SimpleTapBubble.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BubbleBehaviourStrategy
+{
+
+}
+
+public class SimpleTapBubble : MonoBehaviour, IPointerDownHandler
+{
+    private BubbleBehaviourStrategy _strategy;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("1asdasd");
+    }
+}
